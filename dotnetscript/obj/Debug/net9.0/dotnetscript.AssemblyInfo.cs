32b2d519fbef1868489b19b3ead955549c244ff2
Adil67Zoro <adilbakyt67@gmail.com>
2025-06-23 18:07:24 +0500

    fixed raion names

--- dotnetscript/obj/Debug/net9.0/dotnetscript.AssemblyInfo.cs
+++ dotnetscript/obj/Debug/net9.0/dotnetscript.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnetscript")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2de7a5dcd9919ac78935cbfb9e15d10c8eda3c41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c3e5f31c3fc7ed75f99097dac9045626fdeec9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnetscript")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnetscript")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
